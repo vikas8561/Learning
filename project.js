a8c0e3fa7e7c2eb709f157a6171e0a4ed37d1942
vikas kumar <vikaskumarkrishna01@gmail.com>
2024-11-17 12:34:41 +0530

    second project commit

--- project.js
+++ project.js
@@ -1,7 +1,9 @@
-let arr = [2, 6, 8, 9];
+let arr = [2, 6, 8, 9, -1];
 
 for(let i = 0; i < arr.length; i++){
     if(arr[i] % 2 === 0){
         console.log("Even Number");
+    }else if(arr[i] % 2 !== 0){
+        console.log("Odd Number");
     }
 }
